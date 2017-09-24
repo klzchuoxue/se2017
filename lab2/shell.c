340b37dc9343c84cc0ac4124de89272489d7921c
klzchuoxue <31929399+klzchuoxue@users.noreply.github.com>
2017-09-25 00:39:39 +0800

    make a change

--- lab2/shell.c
+++ lab2/shell.c
@@ -1,17 +1,46 @@
-#include <sys/types.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-
-int main()
-{
+#include<stdio.h> 
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/types.h>
+#define NUM 1024
+#define SIZE 50
+int mystrtok(char *argv[], char* string){
+	int i=0;
+	char delim[]=" ";
+	char *p;
+	argv[0] = strtok(string,delim);
+	while(argv[i]!=NULL){
+	argv[++i] = strtok(NULL,delim);
+	// printf("%d is %s\n",i,argv[i]);
+	}
+	return 0;
+}
+int main(){
+	char str[NUM];
+	int status;
 	pid_t pid;
-	pid = fork();
-	if(pid==0)
-		printf("Child process\n");
-	else if(pid >0)
-		printf("Parent process\n");
-	else
-		printf("fork failure\n");
-	exit(0);
+	char * argv[NUM];
+	while(1){
+		printf("\n$:");
+		gets(str);
+		status = mystrtok(argv,str);
+		if(status!=0){
+		printf("fail to get command!\n");
+		}
+		pid = fork();
+		if(pid==-1) {
+			printf("fork failure!\n");
+		}
+		else if(pid==0) {
+
+			if(execvp(argv[0], argv) == -1){
+				perror("execv\n");
+				exit(-1);
+			}
+		}
+		else {
+			wait();
+		}
+	}
 }
